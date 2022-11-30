494ef2d4705072db630c6dbc7f507e180794e0a2
athulya2000 <athulyav00@gmail.com>
2022-11-30 10:15:42 +0530

    addfriends

--- src/app/add-friend/add-friend.component.ts
+++ src/app/add-friend/add-friend.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-add-friend',
@@ -12,9 +13,27 @@ export class AddFriendComponent {
   friendNickName=""
   DescribeYourFriend=""
 
+  constructor(private api:ApiService){
+
+  }
+
   readValue=()=>
   {
     let data:any={"name":this.name,"friendName":this.friendName,"friendNickName":this.friendNickName,"DescribeYourFriend":this.DescribeYourFriend}
     console.log(data)
+    this.api.addFriends(data).subscribe(
+      (response:any)=>{
+        console.log(response)
+        if (response.status=="success") {
+          alert("Friends added successfully")
+          this.name=""
+          this.friendName=""
+          this.DescribeYourFriend=""
+          this.friendNickName
+        } else {
+          alert("something went wrong")
+        }
+      }
+    )
   }
 }
